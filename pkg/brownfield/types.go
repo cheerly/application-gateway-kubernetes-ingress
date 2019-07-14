64a28325623fc31c6f26d6aa299747e4602631b7
Delyan Raychev <delyan.raychev@microsoft.com>
2019-07-13 21:07:02 -0700

    brownfield: Apply blacklists to Listeners, Certificates, Frontend Ports

--- pkg/brownfield/types.go
+++ pkg/brownfield/types.go
@@ -6,22 +6,12 @@
 package brownfield
 
 import (
-	ptv1 "github.com/Azure/application-gateway-kubernetes-ingress/pkg/apis/azureingressprohibitedtarget/v1"
+	"github.com/Azure/application-gateway-kubernetes-ingress/pkg/apis/azureingressprohibitedtarget/v1"
 	n "github.com/Azure/azure-sdk-for-go/services/network/mgmt/2018-12-01/network"
 )
 
-// PoolContext is the basket of App Gateway configs necessary to determine what settings should be
-// managed and what should be left as-is.
-type PoolContext struct {
-	Listeners          []n.ApplicationGatewayHTTPListener
-	RoutingRules       []n.ApplicationGatewayRequestRoutingRule
-	PathMaps           []n.ApplicationGatewayURLPathMap
-	BackendPools       []n.ApplicationGatewayBackendAddressPool
-	ProhibitedTargets  []*ptv1.AzureIngressProhibitedTarget
-	DefaultBackendPool n.ApplicationGatewayBackendAddressPool
-}
-
 type listenerName string
+
 type pathmapName string
 type poolToTargets map[backendPoolName][]Target
 
@@ -41,7 +31,7 @@ type ExistingResources struct {
 	HTTPSettings       []n.ApplicationGatewayBackendHTTPSettings
 	Ports              []n.ApplicationGatewayFrontendPort
 	Probes             []n.ApplicationGatewayProbe
-	ProhibitedTargets  []*ptv1.AzureIngressProhibitedTarget
+	ProhibitedTargets  []*v1.AzureIngressProhibitedTarget
 	DefaultBackendPool *n.ApplicationGatewayBackendAddressPool
 
 	// Cache helper structs
@@ -50,7 +40,7 @@ type ExistingResources struct {
 }
 
 // NewExistingResources creates a new ExistingResources struct.
-func NewExistingResources(appGw n.ApplicationGateway, prohibitedTargets []*ptv1.AzureIngressProhibitedTarget, defaultPool *n.ApplicationGatewayBackendAddressPool) ExistingResources {
+func NewExistingResources(appGw n.ApplicationGateway, prohibitedTargets []*v1.AzureIngressProhibitedTarget, defaultPool *n.ApplicationGatewayBackendAddressPool) ExistingResources {
 	var allExistingSettings []n.ApplicationGatewayBackendHTTPSettings
 	if appGw.BackendHTTPSettingsCollection != nil {
 		allExistingSettings = *appGw.BackendHTTPSettingsCollection
